2ab54d09a97a9c5490c71b30f57ee7e9fdd503c0
mehedi3635 <mh95211@gmail.com>
2026-03-29 20:30:20 +0600

    hi
    hi

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('post/<int:id>/', views.post_detail, name='post_detail'),
+
+    path('create/', views.create_post, name='create_post'),
+    path('edit/<int:id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:id>/', views.delete_post, name='delete_post'),
+
+    path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
+
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .models import Comment
 
-# Create your views here.
+def register_view(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+# block Crud
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
+
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        Post.objects.create(title=title, content=content, author=request.user)
+        return redirect('home')
+    return render(request, 'create_post.html')
+
+
+@login_required
+def edit_post(request, id):
+    post = Post.objects.get(id=id)
+    if post.author == request.user:
+        if request.method == "POST":
+            post.title = request.POST['title']
+            post.content = request.POST['content']
+            post.save()
+            return redirect('home')
+    return render(request, 'edit_post.html', {'post': post})
+
+
+@login_required
+def delete_post(request, id):
+    post = Post.objects.get(id=id)
+    if post.author == request.user:
+        post.delete()
+    return redirect('home')
+
+# comment syestem
+
+@login_required
+def add_comment(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == "POST":
+        content = request.POST['content']
+        Comment.objects.create(post=post, user=request.user, content=content)
+    return redirect('post_detail', post_id)
+
+
+# Like Syestem
+
+@login_required
+def like_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if request.user in post.likes.all():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+
+    return redirect('post_detail', post_id)
+
+# post details
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    comments = post.comments.all()
+
+    return render(request, 'post_detail.html', {
+        'post': post,
+        'comments': comments
+    })
